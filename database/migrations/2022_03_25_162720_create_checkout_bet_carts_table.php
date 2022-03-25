016db9e2392d0511aa532c03feb66cc668636d78
josephkipkemoi <jkemboe@gmail.com>
2022-03-25 19:51:31 +0300

    Checkout Database

---
+++ database/migrations/2022_03_25_162720_create_checkout_bet_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkout_bet_carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('session_id');
+            $table->bigInteger('stake_amount');
+            $table->bigInteger('total_odds');
+            $table->bigInteger('final_payout');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkout_bet_carts');
+    }
+};
