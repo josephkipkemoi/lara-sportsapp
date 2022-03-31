baa79a36162b12ebdcbe8a875e28a8307f51048c
josephkipkemoi <jkemboe@gmail.com>
2022-03-31 21:45:33 +0300

    Checkout refactor

--- database/migrations/2022_03_25_162720_create_checkout_bet_carts_table.php
+++ database/migrations/2022_03_25_162720_create_checkout_bet_carts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('checkout_bet_carts', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('session_id');
             $table->bigInteger('stake_amount');
             $table->bigInteger('total_odds');
